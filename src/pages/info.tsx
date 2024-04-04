72cd9c4b145fd22e3ec7e656fbb81d2cf2d342c0
taehwan heo <htth815@gmail.com>
2024-04-04 10:44:03 +0900

    index, info tsx파일 작성

--- src/pages/info.tsx
+++ src/pages/info.tsx
@@ -1,7 +1,7 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
-import Text from 'components/Text'
-import { Link } from 'gatsby'
+import { Global, css } from '@emotion/react'
+import styled from '@emotion/styled'
 
 
 type InfoPageProps = {
@@ -16,6 +16,37 @@ type InfoPageProps = {
   }
 }
 
+const globalStyle = css`
+    * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+        font-size: 20px;
+    }
+`
+
+const TextStyle = css`
+    font-size: 18px;
+    font-weight: 700;
+    color: gray;
+`
+
+// Styled Component 생성 : 사용자 정의 스타일 적용된 Div 요소
+// Kebab Case 적용
+const Text1 = styled.div<{ disable: boolean}>`
+    font-size: 20px;
+    font-weight: 700;
+    text-decoration: ${({ disable }) => (disable ? 'line-through' : 'none')};
+`
+
+// Camel Case 적용
+const Text2 = styled('div')<{ disable: boolean }>(({ disable }) => ({
+    fontSize: '15px',
+    color: 'blue',
+    textDecoration: disable ? 'line-through' : 'none',
+}))
+
+
 const InfoPage: FunctionComponent<InfoPageProps> = function ({
   data: {
     site: {
@@ -25,10 +56,10 @@ const InfoPage: FunctionComponent<InfoPageProps> = function ({
 }) {
   return (
     <div>
-      <Text text={title} />
-      <Text text={description} />
-      <Text text={author} />
-      <Link to="/">To Main</Link>
+        <Global styles={globalStyle} />
+        <div css={TextStyle}>{title}</div>
+        <Text1 disable={true}>{description}</Text1>
+        <Text2 disable={true}>{author}</Text2>
     </div>
   )
 }
@@ -45,4 +76,4 @@ export const metadataQuery = graphql`
       }
     }
   }
-`;
+`
