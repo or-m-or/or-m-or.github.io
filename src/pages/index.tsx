a4484847e4486f529b1dd36ef309edd841d8ce69
taehwan heo <htth815@gmail.com>
2024-04-06 15:56:25 +0900

    카테고리 생성 및 카테고리별로 포스트 아이템 띄워주기 완료

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react'
+import React, { FunctionComponent, useMemo } from 'react'
 import styled from '@emotion/styled'
 import GlobalStyle from 'components/Common/GlobalStyle'
 import Footer from 'components/Common/Footer'
@@ -8,9 +8,13 @@ import PostList, { PostType } from 'components/Main/PostList'
 import { graphql } from 'gatsby'
 import { IGatsbyImageData } from 'gatsby-plugin-image'
 import { PostListItemType } from 'types/PostItem.types'
+import queryString, { ParsedQuery} from 'query-string'
 
 
 type IndexPageProps = {
+    location: {
+        search: string
+    }
     data: {
         allMarkdownRemark: {
             edges: PostListItemType[]
@@ -23,18 +27,15 @@ type IndexPageProps = {
     }
 }
 
-const CATEGORY_LIST = {
-    All: 5,
-    Web: 3,
-    Mobile: 2,
-}
 
 const Container = styled.div`
     display: flex;
     flex-direction: column;
     height: 100%;
 `
+
 const IndexPage: FunctionComponent<IndexPageProps> = function ({
+    location: { search },
     data: {
         allMarkdownRemark: { edges },
         file: {
@@ -42,12 +43,46 @@ const IndexPage: FunctionComponent<IndexPageProps> = function ({
         },
     },
 }) {
+    const parsed: ParsedQuery<string> = queryString.parse(search)
+    const selectedCategory: string =
+        typeof parsed.category !== 'string' || !parsed.category
+            ? 'All'
+            : parsed.category
+
+    const categoryList = useMemo(
+        () =>
+            edges.reduce(
+                (
+                    list: CategoryListProps['categoryList'],
+                    {
+                        node: {
+                            frontmatter: { categories },
+                        },
+                    }: PostType,
+                ) => {
+                    categories.forEach(category => {
+                        if (list[category] === undefined) list[category] = 1;
+                        else list[category]++;
+                    });
+        
+                    list['All']++;
+        
+                    return list;
+                },
+                { All: 0 },
+            ),
+        [],
+    )
+
     return (
         <Container>
             <GlobalStyle />
-            <Introduction profileImage={gatsbyImageData}/>
-            <CategoryList selectedCategory="Web" categoryList={CATEGORY_LIST} />
-            <PostList posts={edges} />
+            <Introduction profileImage={gatsbyImageData} />
+            <CategoryList
+                selectedCategory={selectedCategory}
+                categoryList={categoryList}
+            />
+            <PostList selectedCategory={selectedCategory} posts={edges} />
             <Footer />
         </Container>
     )
