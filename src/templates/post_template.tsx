92befa2bfe3ef225ae24c3d9bd740cae537d952f
taehwan heo <htth815@gmail.com>
2024-04-06 20:24:31 +0900

    게시글 페이지 구현하기-마크다운 형식의 포스트 출력하기

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -1,13 +1,49 @@
 import React, { FunctionComponent } from 'react'
 import { graphql } from 'gatsby'
+import { PostPageItemType } from 'types/PostItem.types' // 이후 정의
 import Template from 'components/Common/Template'
+import PostHead from 'components/Post/PostHead'
+import PostContent from 'components/Post/PostContent'
 
-type PostTemplateProps = {}
+type PostTemplateProps = {
+  data: {
+    allMarkdownRemark: {
+      edges: PostPageItemType[] // 존재하지 않는 타입이므로 에러가 발생하지만 일단 작성
+    }
+  }
+}
 
-const PostTemplate: FunctionComponent<PostTemplateProps> = function (props) {
-  console.log(props)
+const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
+  data: {
+    allMarkdownRemark: { edges },
+  },
+}) {
+  const {
+    node: {
+      html,
+      frontmatter: {
+        title,
+        summary,
+        date,
+        categories,
+        thumbnail: {
+          childImageSharp: { gatsbyImageData },
+        },
+      },
+    },
+  } = edges[0]
 
-  return <Template>Post Template</Template>
+  return (
+    <Template>
+      <PostHead
+        title={title}
+        date={date}
+        categories={categories}
+        thumbnail={gatsbyImageData}
+      />
+      <PostContent html={html} />
+    </Template>
+  )
 }
 
 export default PostTemplate
@@ -34,3 +70,9 @@ export const queryMarkdownDataBySlug = graphql`
     }
   }
 `
+export type PostPageItemType = {
+  node: {
+    html: string
+    frontmatter: PostFrontmatterType
+  }
+}
