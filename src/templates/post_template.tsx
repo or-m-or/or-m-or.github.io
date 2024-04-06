5b2d9ae249738aa3889f5c2b89872f4e9ff077cd
taehwan heo <htth815@gmail.com>
2024-04-06 20:51:02 +0900

    게시글 페이지 구현하기-게시글 페이지 반응형 디자인 구현하기 완료

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -4,6 +4,7 @@ import { PostPageItemType } from 'types/PostItem.types' // 이후 정의
 import Template from 'components/Common/Template'
 import PostHead from 'components/Post/PostHead'
 import PostContent from 'components/Post/PostContent'
+import CommentWidget from 'components/Post/CommentWidget'
 
 type PostTemplateProps = {
   data: {
@@ -19,29 +20,14 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
   },
 }) {
   const {
-    node: {
-      html,
-      frontmatter: {
-        title,
-        summary,
-        date,
-        categories,
-        thumbnail: {
-          childImageSharp: { gatsbyImageData },
-        },
-      },
-    },
+    node: { html, frontmatter },
   } = edges[0]
 
   return (
     <Template>
-      <PostHead
-        title={title}
-        date={date}
-        categories={categories}
-        thumbnail={gatsbyImageData}
-      />
+      <PostHead {...frontmatter} />
       <PostContent html={html} />
+      <CommentWidget />
     </Template>
   )
 }
